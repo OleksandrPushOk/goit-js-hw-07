cc2f5de92570d81f47ee86bc85f089c8f110fec6
OleksandrPushOk <koshup90@gmail.com>
2022-09-01 18:53:34 +0300

    I did my homework

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// console.log(galleryItems);
 
-console.log(galleryItems);
+const itemEl = document.querySelector('.gallery');
+
+const galleryEl = galleryItems
+.map(({ preview, description, original }) =>
+`<div class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>
+</div>`)
+.join('');
+
+itemEl.insertAdjacentHTML('beforeend', galleryEl)
+
+itemEl.addEventListener('click', onClickImage)
+
+function onClickImage(event) {
+    event.preventDefault();
+
+    if (event.target.nodeName !== 'IMG') {
+     return;
+    }
+
+    const modal =  basicLightbox.create(
+        `<img src="${event.target.dataset.sourse}" width="800" height="600">`,
+    
+        {
+            onShow:() => window.addEventListener('keydown', onEscPress),
+            onclose:() => window. removeEventListener('keydown', onEscPress),
+        }
+    );
+
+    modal.show()
+
+    function onEscPress(event) {
+        if (event.code === 'Escape') {
+            modal.close();
+        }
+    }
+}
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,15 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const itemEl = document.querySelector('.gallery');
+
+const galerryEl = galleryItems.map(({preview, original, description}) =>
+` <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+ </a>`).join('');
+ 
+ itemEl.insertAdjacentHTML("afterbegin", galerryEl)
+ const lightbox = new SimpleLightbox('.gallery a');
+ console.log(lightbox);
